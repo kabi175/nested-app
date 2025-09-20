fd501d95c6165cafe6990ee32bd7003844535c78
kabi175 <kabilan.mee@gmail.com>
2025-09-21 02:12:40 +0530

    Add WebFlux support and testing dependencies

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
 	// Swagger/OpenAPI dependencies
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
