8bb8be3a466822a161bbe36b3e56349540e3c3f1
kabi175 <kabilan.mee@gmail.com>
2025-09-21 02:22:06 +0530

    Update refreshDetails method to schedule daily execution and improve error handling

--- server/src/main/java/com/nested/app/nse/FundServiceImpl.java
+++ server/src/main/java/com/nested/app/nse/FundServiceImpl.java
@@ -16,21 +16,17 @@ public class FundServiceImpl implements FundService {
 
     private static final String FUND_DETAILS_ENDPOINT = "/nsemfdesk/api/v2/reports/MASTER_DOWNLOAD";
 
-    @Scheduled(fixedDelay = 1000000)
+    @Scheduled(fixedDelay = 1000 * 60 * 60 * 24) // Refresh every 24 hours
     @Override
     public void refreshDetails() {
-       var content =  nseAPIBuild.build(true).post().uri(FUND_DETAILS_ENDPOINT).bodyValue(Map.of("file_type", "SCH")).retrieve()
-               .onStatus(HttpStatus.BAD_REQUEST::equals, response ->
+        var content = nseAPIBuild.build(true).post().uri(FUND_DETAILS_ENDPOINT).bodyValue(Map.of("file_type", "SCH")).retrieve().onStatus(HttpStatus.BAD_REQUEST::equals, response ->
 
-                       response.bodyToMono(String.class)
-                               .flatMap(errorBody -> Mono.error(
-                                       new RuntimeException("400 Bad Request: " + errorBody + "-" + response.request().getURI())))
-               )
-               .bodyToMono(String.class).block();
+                response.bodyToMono(String.class).flatMap(errorBody -> Mono.error(new RuntimeException("400 Bad Request: " + errorBody + "-" + response.request().getURI())))).bodyToMono(String.class).block();
 
         assert content != null;
         var headers = content.lines().findFirst().orElseThrow().split("\\|");
 
         content.lines().map(line -> line.split("\\|"));
+        //TODO: Store the details in DB
     }
 }
