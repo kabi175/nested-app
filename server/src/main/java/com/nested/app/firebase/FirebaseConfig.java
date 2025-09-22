31eca042a473e6f574d9604f3c3dc75a2d4fe00a
kabi175 <kabilan.mee@gmail.com>
2025-09-23 01:13:31 +0530

    Refactor Firebase configuration to use ResourceLoader for service account key; update .gitignore to exclude serviceAccountKey.json

--- server/src/main/java/com/nested/app/firebase/FirebaseConfig.java
+++ server/src/main/java/com/nested/app/firebase/FirebaseConfig.java
@@ -3,18 +3,23 @@ package com.nested.app.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
+@AllArgsConstructor
 public class FirebaseConfig {
+
+    private final ResourceLoader resourceLoader;
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream(System.getenv("GOOGLE_APPLICATION_CREDENTIALS"));
+        FileInputStream serviceAccount = new FileInputStream(resourceLoader.getResource("classpath:serviceAccountKey.json").getFile());
 
         FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
 
