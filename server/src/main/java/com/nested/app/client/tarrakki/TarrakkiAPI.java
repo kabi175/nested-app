16fadc0f7d317ab7415566996245ada0fbe66b38
kabi175 <kabilan.mee@gmail.com>
2025-10-02 19:05:42 +0530

    Rename TarrakkiService to TarrakkiAPI and refactor authentication handling
    Signed-off-by: kabi175 <kabilan.mee@gmail.com>

---
+++ server/src/main/java/com/nested/app/client/tarrakki/TarrakkiAPI.java
@@ -0,0 +1,30 @@
+package com.nested.app.client.tarrakki;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/** Service for interacting with Tarrakki API */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TarrakkiAPI {
+
+  private final WebClient webClient;
+  private final TarrakkiTokenProvider tokenProvider;
+
+  @Value("${tarrakki.api.base-url}")
+  private final String baseUrl;
+
+  private WebClient withAuth() {
+    String token = tokenProvider.getToken();
+    var authToken = "Bearer " + token;
+    return WebClient.builder()
+        .baseUrl(baseUrl)
+        .defaultHeader(HttpHeaders.AUTHORIZATION, authToken)
+        .build();
+  }
+}
