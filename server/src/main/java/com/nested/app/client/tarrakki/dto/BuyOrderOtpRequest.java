19d96eac708b91defd1b7c586503063c3a6669e9
kabi175 <kabilan.mee@gmail.com>
2025-10-19 09:12:52 +0530

    feat: add BuyOrderOtpRequest DTO for handling buy order OTP requests
    Signed-off-by: kabi175 <kabilan.mee@gmail.com>

---
+++ server/src/main/java/com/nested/app/client/tarrakki/dto/BuyOrderOtpRequest.java
@@ -0,0 +1,24 @@
+package com.nested.app.client.tarrakki.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class BuyOrderOtpRequest extends OtpRequest {
+  String fundID;
+  String folio;
+
+  public static BuyOrderOtpRequest getInstance(String investorID, String fundID) {
+    return getInstance(investorID, fundID, null);
+  }
+
+  public static BuyOrderOtpRequest getInstance(String investorID, String fundID, String folio) {
+    BuyOrderOtpRequest request = new BuyOrderOtpRequest();
+    request.setOtp_type(Type.BUY_ORDER);
+    request.setInvestor_id(investorID);
+    request.setFundID(fundID);
+    request.setFolio(folio);
+    return request;
+  }
+}
