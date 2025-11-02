da18b16e718432eecbc52c77e7ac2988fa3f7225
kabi175 <kabilan.mee@gmail.com>
2025-11-03 02:06:56 +0530

    refactor: update import for Text component in HomeScreen
    - Changed the import of the Text component from "lucide-react-native" to "react-native" for consistency and to utilize the standard React Native library.

--- mobile-app/app/(tabs)/index.tsx
+++ mobile-app/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Text } from "lucide-react-native";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
