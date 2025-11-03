ef0e02701ea276c810c5787855ba19da2d46ad41
kabi175 <kabilan.mee@gmail.com>
2025-11-03 14:52:59 +0530

    feat: add UPIButton component with UPI image
    - Introduced a new UPIButton component that displays a UPI image and handles press events.
    - Added UPI image asset to the project for use in the button.

---
+++ mobile-app/components/buttons/UPIButton.tsx
@@ -0,0 +1,28 @@
+import { Image, Pressable, StyleSheet } from "react-native";
+
+export default function UPIButton({ onPress }: { onPress?: () => void }) {
+  return (
+    <Pressable style={styles.upiImageContainer} onPress={onPress}>
+      <Image
+        source={require("@/assets/images/UPI.png")}
+        style={styles.upiImage}
+      />
+    </Pressable>
+  );
+}
+const styles = StyleSheet.create({
+  upiImageContainer: {
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#E5E7EB",
+    padding: 10,
+    height: 80,
+    width: 150,
+    marginBottom: 20,
+  },
+  upiImage: {
+    height: "100%",
+    width: "100%",
+    resizeMode: "contain",
+  },
+});
