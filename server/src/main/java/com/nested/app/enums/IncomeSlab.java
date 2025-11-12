ade918517653ddaafd82483cbfc75ff5f5433728
kabi175 <kabilan.mee@gmail.com>
2025-11-12 22:39:21 +0530

    feat: update income slab labels for consistency and clarity
    Signed-off-by: kabi175 <kabilan.mee@gmail.com>

--- server/src/main/java/com/nested/app/enums/IncomeSlab.java
+++ server/src/main/java/com/nested/app/enums/IncomeSlab.java
@@ -4,12 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum IncomeSlab {
-    BELOW_1_LAC("below_1_lac"),
-    ABOVE_1_LAC_UPTO_5_LAC("above_1_lac_upto_5_lac"),
-    ABOVE_5_LAC_UPTO_10_LAC("above_5_lac_upto_10_lac"),
-    ABOVE_10_LAC_UPTO_25_LAC("above_10_lac_upto_25_lac"),
-    ABOVE_25_LAC_UPTO_1_CRORE("above_25_lac_upto_1_crore"),
-    ABOVE_1_CRORE("above_1_crore");
+  BELOW_1_LAC("upto_1lakh"),
+  ABOVE_1_LAC_UPTO_5_LAC("above_1lakh_upto_5lakh"),
+  ABOVE_5_LAC_UPTO_10_LAC("above_5lakh_upto_10lakh"),
+  ABOVE_10_LAC_UPTO_25_LAC("above_10lakh_upto_25lakh"),
+  ABOVE_25_LAC_UPTO_1_CRORE("above_25lakh_upto_1cr"),
+  ABOVE_1_CRORE("above_1cr");
 
     private final String value;
 
