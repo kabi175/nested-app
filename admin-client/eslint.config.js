a1f09cf34b1f3a9c319315249b8a51e11f64412a
kabi175 <kabilan.mee@gmail.com>
2025-12-06 14:31:31 +0530

    refactor: comment out unused ESLint configuration for better clarity

--- admin-client/eslint.config.js
+++ admin-client/eslint.config.js
@@ -1,26 +1,25 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
+// import js from "@eslint/js";
+// import ts from "@typescript-eslint/eslint-plugin";
+// import tsParser from "@typescript-eslint/parser";
 // import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      // "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      "@typescript-eslint/no-unused-vars": "off",
-    },
-  },
-);
+// export default [
+//   js.configs.recommended,
+//   {
+//     files: ["**/*.ts", "**/*.tsx"],
+//     languageOptions: {
+//       parser: tsParser,
+//       parserOptions: {
+//         project: "./tsconfig.json",
+//       },
+//     },
+//     plugins: {
+//       "@typescript-eslint": ts,
+//       "react-refresh": reactRefresh,
+//     },
+//     rules: {
+//       "@typescript-eslint/no-unused-vars": "warn",
+//       "react-refresh/only-export-components": "warn",
+//     },
+//   },
+// ];
