6b3bd12562594ad8299a01339b3279caf40854d5
kabi175 <kabilan.mee@gmail.com>
2025-12-09 01:01:44 +0530

    refactor: remove unused methods from ChildRepository to streamline code
    Signed-off-by: kabi175 <kabilan.mee@gmail.com>

--- server/src/main/java/com/nested/app/repository/ChildRepository.java
+++ server/src/main/java/com/nested/app/repository/ChildRepository.java
@@ -1,11 +1,9 @@
 package com.nested.app.repository;
 
+import com.nested.app.entity.Child;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.nested.app.entity.Child;
 
 /**
  * Repository interface for Child entity
@@ -24,36 +22,5 @@ public interface ChildRepository extends JpaRepository<Child, Long> {
      * @return List of children for the specified user
      */
     List<Child> findByUserId(Long userId);
-    
-    /**
-     * Find children by first name
-     * 
-     * @param firstName First name to search for
-     * @return List of children with the specified first name
-     */
-    List<Child> findByFirstName(String firstName);
-    
-    /**
-     * Find children by last name
-     * 
-     * @param lastName Last name to search for
-     * @return List of children with the specified last name
-     */
-    List<Child> findByLastName(String lastName);
-    
-    /**
-     * Find children by gender
-     * 
-     * @param gender Gender to search for
-     * @return List of children with the specified gender
-     */
-    List<Child> findByGender(String gender);
-    
-    /**
-     * Find children by invest under child flag
-     * 
-     * @param investUnderChild Flag indicating if investment is under child
-     * @return List of children with the specified investment flag
-     */
-    List<Child> findByInvestUnderChild(boolean investUnderChild);
+
 }
