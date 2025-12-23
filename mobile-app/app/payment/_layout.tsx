b6635150ace7bc90e96f35fe4be4f498a1c09689
kabi175 <kabilan.mee@gmail.com>
2025-12-23 16:14:07 +0530

    refactor: remove unused import of useSafeAreaInsets in payment layout for cleaner code

--- mobile-app/app/payment/_layout.tsx
+++ mobile-app/app/payment/_layout.tsx
@@ -3,11 +3,9 @@ import { userAtom } from "@/atoms/user";
 import { Redirect, Stack } from "expo-router";
 import { useAtomValue } from "jotai";
 import { useEffect } from "react";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Layout() {
   const user = useAtomValue(userAtom);
-  const insets = useSafeAreaInsets();
 
   useEffect(() => {
     if (user && !user.is_ready_to_invest) {
