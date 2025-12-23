3e7e08ffe766f1825f6a6fa17c9d55514b1649ab
kabi175 <kabilan.mee@gmail.com>
2025-12-23 20:27:55 +0530

    refactor: remove unnecessary @Service annotation from BuyOrderApiClient for cleaner interface definition
    Signed-off-by: kabi175 <kabilan.mee@gmail.com>

--- server/src/main/java/com/nested/app/client/mf/BuyOrderApiClient.java
+++ server/src/main/java/com/nested/app/client/mf/BuyOrderApiClient.java
@@ -5,10 +5,8 @@ import com.nested.app.client.mf.dto.OrderConsentRequest;
 import com.nested.app.client.mf.dto.OrderData;
 import com.nested.app.client.mf.dto.OrderDetail;
 import java.util.List;
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-@Service
 public interface BuyOrderApiClient {
 
   Mono<Void> updateConsent(OrderConsentRequest confirmOrderRequest);
