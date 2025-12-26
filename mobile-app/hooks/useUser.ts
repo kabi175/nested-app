8cb4659bece4de3b446fd5ee08cb9d43c70bad84
kabi175 <kabilan.mee@gmail.com>
2025-12-26 05:34:20 +0530

    feat: integrate user state management with API data retrieval
    - Added userAtom for state management using Jotai.
    - Enhanced useUser hook to set user data in state upon successful API retrieval.
    - Implemented useEffect to synchronize user data with the application state, improving data consistency.

--- mobile-app/hooks/useUser.ts
+++ mobile-app/hooks/useUser.ts
@@ -1,10 +1,22 @@
 import { getUser } from "@/api/userApi";
+import { userAtom } from "@/atoms/user";
 import { QUERY_KEYS } from "@/constants/queryKeys";
 import { useQuery } from "@tanstack/react-query";
+import { useSetAtom } from "jotai";
+import { useEffect } from "react";
 
 export function useUser() {
-  return useQuery({
+  const setUser = useSetAtom(userAtom);
+  const query = useQuery({
     queryKey: [QUERY_KEYS.user],
     queryFn: () => getUser(),
   });
+
+  useEffect(() => {
+    if (query.data !== undefined) {
+      setUser(query.data);
+    }
+  }, [query.data, setUser]);
+
+  return query;
 }
