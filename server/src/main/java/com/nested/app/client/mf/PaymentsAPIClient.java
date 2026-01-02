9a2170b03ed407a898006ed1df68359104e69ead
kabi175 <kabilan.mee@gmail.com>
2026-01-02 23:55:03 +0530

    refactor: remove Spring @Service annotation from PaymentsAPIClient interface
    Signed-off-by: kabi175 <kabilan.mee@gmail.com>

--- server/src/main/java/com/nested/app/client/mf/PaymentsAPIClient.java
+++ server/src/main/java/com/nested/app/client/mf/PaymentsAPIClient.java
@@ -2,11 +2,9 @@ package com.nested.app.client.mf;
 
 import com.nested.app.client.mf.dto.PaymentsRequest;
 import com.nested.app.client.mf.dto.PaymentsResponse;
-import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Mono;
 
-@Service
 public interface PaymentsAPIClient {
   Mono<PaymentsResponse> createPayment(@Validated PaymentsRequest request);
 
