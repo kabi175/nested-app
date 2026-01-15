3f11bb64b895ddc72bbb66ff2b31042cc696c268
kabi175 <kabilan.mee@gmail.com>
2026-01-15 10:41:07 +0530

    fix: correct credential expiration calculation and improve error handling in useAuthAxios hook

--- mobile-app/hooks/useAuthAxios.ts
+++ mobile-app/hooks/useAuthAxios.ts
@@ -1,32 +1,36 @@
 // useAuthAxios.ts
 import { api } from "@/api/client";
+import { clearNomineeAtoms } from "@/utils/nominee";
+import { useQueryClient } from "@tanstack/react-query";
 import { router } from "expo-router";
 import { useEffect } from "react";
 import { CredentialsManagerError, useAuth0 } from "react-native-auth0";
 
 export function useAuthAxios() {
   const { getCredentials } = useAuth0();
+  const queryClient = useQueryClient();
 
   useEffect(() => {
     const interceptor = api.interceptors.request.use(async (config) => {
       try {
         let credential = await getCredentials();
-        let expiresAt = credential.expiresAt + 19800000;
+        let expiresAt = credential.expiresAt + 19800;
         const now = Math.floor(Date.now() / 1000);
-        if (expiresAt >= now) {
-          console.log("credential expired, refreshing");
+        if (now >= expiresAt) {
+          console.log("credential expired, refreshing", expiresAt, now);
           credential = await getCredentials(
             credential.scope,
-            1 * 24 * 60 * 60, // min ttl is 1 day
+            undefined, // min ttl is 1 day
             undefined,
             true
           );
         }
         config.headers.Authorization = `Bearer ${credential?.idToken}`;
-        console.log("credential", credential);
       } catch (error) {
         console.log("error during credential refresh", error);
         if (error instanceof CredentialsManagerError) {
+          queryClient.clear();
+          clearNomineeAtoms();
           router.replace("/sign-in");
         }
       }
