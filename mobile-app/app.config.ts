c61f829319e84f671f78b1a685b8d114dd3138e5
kabi175 <kabilan.mee@gmail.com>
2026-02-01 23:28:42 +0530

    chore: bump version from 1.4.0 to 1.4.1 in app.config.ts, app.json, and package.json

--- mobile-app/app.config.ts
+++ mobile-app/app.config.ts
@@ -3,7 +3,7 @@ module.exports = () => ({
     name: "NestEd",
     slug: "nested",
     owner: "kabi175",
-    version: "1.4.0",
+    version: "1.4.1",
     icon: "./assets/images/icon.png",
     scheme: "nested",
     android: {
