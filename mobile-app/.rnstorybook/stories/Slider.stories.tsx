0e48dec42baa4523703739851b31bebef24c5dda
kabi175 <kabilan.mee@gmail.com>
2026-03-11 01:37:55 +0530

    feat: implement v2 Slider component and add its Storybook story

---
+++ mobile-app/.rnstorybook/stories/Slider.stories.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Slider from '../../components/v2/Slider';
+
+export default {
+  title: 'v2/Slider',
+  component: Slider,
+};
+
+export const Default = () => {
+  const [value, setValue] = useState(5840);
+  return (
+    <View style={styles.container}>
+      <Slider
+        min={3000}
+        max={11000}
+        initialValue={5840}
+        step={10}
+        onValueChange={setValue}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+});
