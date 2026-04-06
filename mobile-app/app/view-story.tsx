4c10f5816ae486150999faa7b8243f269f9c12c5
kabi175 <kabilan.mee@gmail.com>
2026-04-06 21:26:43 +0530

    removed duplicate video

--- mobile-app/app/view-story.tsx
+++ mobile-app/app/view-story.tsx
@@ -4,8 +4,7 @@ import React, { useCallback } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 const STORY_VIDEOS = [
-    'https://res.cloudinary.com/dn6qn2gp8/video/upload/v1773954351/WhatsApp_Video_2026-03-17_at_9.01.57_PM_cf42q1.mp4',
-    'https://res.cloudinary.com/dn6qn2gp8/video/upload/v1773954351/WhatsApp_Video_2026-03-17_at_9.01.57_PM_cf42q1.mp4',
+    'https://res.cloudinary.com/dn6qn2gp8/video/upload/v1773954351/WhatsApp_Video_2026-03-17_at_9.01.57_PM_cf42q1.mp4'
 ];
 
 export default function ViewStoryScreen() {
