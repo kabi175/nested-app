56b93106a8a55767be98a5895dd27c71f852b676
kabi175 <kabilan.mee@gmail.com>
2026-04-11 16:34:43 +0530

    fix: route restore issue

--- mobile-app/components/RestoreLastRoute.tsx
+++ mobile-app/components/RestoreLastRoute.tsx
@@ -1,6 +1,7 @@
 // components/RestoreLastRoute.tsx
-import { isBlocked, LAST_ROUTE_KEY } from "@/hooks/usePersistRoute";
 import SplashScreenComponent from "@/components/v2/SplashScreen";
+import { isBlocked, LAST_ROUTE_KEY } from "@/hooks/usePersistRoute";
+import { useRouteRestored } from "@/providers/RouteRestoredProvider";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Redirect } from "expo-router";
 import { useEffect, useState } from "react";
@@ -8,12 +9,14 @@ import { useEffect, useState } from "react";
 
 export default function RestoreLastRoute() {
     const [target, setTarget] = useState<string | null>(null);
+    const { markRestored } = useRouteRestored();
 
     useEffect(() => {
         async function load() {
             const fullRoute = await AsyncStorage.getItem(LAST_ROUTE_KEY);
             if (
                 fullRoute &&
+                fullRoute !== "/" &&
                 !isBlocked(fullRoute)
             ) {
                 console.log("restoring last route", fullRoute);
@@ -22,6 +25,8 @@ export default function RestoreLastRoute() {
                 console.log("no last route found, redirecting to (tabs)");
                 setTarget("/(tabs)"); // fallback
             }
+            console.log("marking route as restored");
+            markRestored();
         }
 
         load();
